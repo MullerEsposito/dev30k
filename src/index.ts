af7b6b2910bb629c513bd2dafba8dc258d383a9d
Muller Esposito <mulleresposito@hotmail.com>
2024-11-01 21:58:07 -0300

    refact(index): it updates the entry point from the api;

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,13 @@
 import * as dotenv from "dotenv"
 import * as StellarSdk from "@stellar/stellar-sdk"
-import { askForNetwork } from "./stellar/utils.js"
-import { createServer, createTransaction } from "./stellar/connection-functions.js"
+import { askForNetwork } from "./utils/prompts.js"
+import "./server.js";
 
 dotenv.config();
 
 (async function main () {
-  const keypar = StellarSdk.Keypair.fromSecret(process.env.SECRET_KEY);
+  // const keypar = StellarSdk.Keypair.fromPublicKey(process.env.PUBLIC_KEY_ACCOUNT_TEST);  
+  // const network = await askForNetwork();
 
-  const message = "DEV30K";
-  const messageInBase64 = btoa(message);
-  const messageInBase64Signed = keypar.sign(Buffer.from(messageInBase64));
   
-  const network = await askForNetwork();
-
-  try {
-    const server = createServer(network);
-
-    const transaction = await createTransaction({ 
-      server, 
-      network, 
-      keypar, 
-      operationName: 'desafio', 
-      operationValue: messageInBase64Signed,
-      memoText: 'DEV30K' 
-    });  
-
-    transaction.send();
-  } catch (error) {
-    console.log(`${error}`);    
-  }
 })();
