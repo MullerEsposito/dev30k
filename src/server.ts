370c09d88756efffb197b30bd2c256149db10d93
Muller Esposito <mulleresposito@hotmail.com>
2024-11-01 22:13:16 -0300

    refact(server): it changes the route from post to get;

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request } from 'express';
 import bodyParser from 'body-parser';
 import * as StellarSdk from '@stellar/stellar-sdk'
 
@@ -10,15 +10,21 @@ import { createTransactionService } from './services/createTransactionService.js
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/verify', async (req, res) => {
-  const { transactionId, sourceMessage, network } = req.body;
+interface IVerifyRequestParams {
+  transactionId: string;
+  sourceMessage: string;
+  network: number;
+}
+
+app.get('/verify', async (req: Request<{}, {}, {}, IVerifyRequestParams>, res) => {
+  const { transactionId, sourceMessage, network } = req.query;
 
   if (!transactionId || !network) {
     res.status(400).json({ error: 'Mensagem e chave secreta são obrigatórias' });
   }
 
   try {
-    const server = createServerService(network as NetworksOptions);
+    const server = createServerService(network as unknown as NetworksOptions);
     const isAuthentic = await verifyDataTransactionService({ transactionId, sourceMessage, server });
 
     res.json({ isAuthentic });
