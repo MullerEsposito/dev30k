700beebf0961438343b1f5896facf389ca83051b
Muller Esposito <mulleresposito@hotmail.com>
2024-11-03 01:36:24 -0300

    refact(Controllers): it modifies the api folder structure to have controllers;

---
+++ src/controllers/verifyDataTransactionController/index.ts
@@ -0,0 +1,47 @@
+import express, { Request } from 'express';
+
+import { NetworksOptions } from '../../utils/enums.js';
+import { createServerService } from '../../services/createServerService.js';
+import { verifyDataTransactionService } from '../../services/verifyDataTransactionService.js';
+
+export const verifyDataTransactionRoute = express.Router();
+
+export const networkMap = {
+    SOROBAN_MAINNET: NetworksOptions.SOROBAN_MAINNET,
+    SOROBAN_TESTNET: NetworksOptions.SOROBAN_TESTNET,
+    HORIZON_MAINNET: NetworksOptions.HORIZON_MAINNET,
+    HORIZON_TESTNET: NetworksOptions.HORIZON_TESTNET,
+};
+
+interface IVerifyRequestParams {
+    transactionId: string;
+    sourceMessage: string;
+    network: NetworksOptions;
+}
+
+verifyDataTransactionRoute.get('/', async (req: Request<{}, {}, {}, IVerifyRequestParams>, res): Promise<void> => {
+    const { transactionId, sourceMessage, network } = req.query;
+
+    const missingFields = [];
+    if (!transactionId) missingFields.push('transactionId');
+    if (!sourceMessage) missingFields.push('sourceMessage');
+    if (!network) missingFields.push('network');
+
+    if (missingFields.length > 0) {
+        res.status(400).json({
+            error: `Campos: ${missingFields.join(', ')} são obrigatórios!`
+        });
+        return;
+    }
+
+    try {
+        const server = createServerService(networkMap[network]);
+        const isAuthentic = await verifyDataTransactionService({ transactionId, sourceMessage, server });
+
+        res.status(200).json({ isAuthentic });
+        return;
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao verificar dados da transação', details: error.message });
+        return;
+    }
+});
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,15 @@
-import express, { Request } from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import * as StellarSdk from '@stellar/stellar-sdk'
+import swaggerUi from 'swagger-ui-express';
 
-import { verifyDataTransactionService } from './services/verifyDataTransactionService.js';
-import { createServerService } from './services/createServerService.js';
-import { NetworksOptions } from './utils/enums.js';
-import { createTransactionService } from './services/createTransactionService.js';
+import swaggerDocs from './docs/swaggerConfig.js';
+import { verifyDataTransactionRoute } from './controllers/verifyDataTransactionController/index.js';
 
 const app = express();
 app.use(bodyParser.json());
 
-interface IVerifyRequestParams {
-  transactionId: string;
-  sourceMessage: string;
-  network: number;
-}
-
-app.get('/verify', async (req: Request<{}, {}, {}, IVerifyRequestParams>, res) => {
-  const { transactionId, sourceMessage, network } = req.query;
-
-  if (!transactionId || !network) {
-    res.status(400).json({ error: 'Mensagem e chave secreta são obrigatórias' });
-  }
-
-  try {
-    const server = createServerService(network as unknown as NetworksOptions);
-    const isAuthentic = await verifyDataTransactionService({ transactionId, sourceMessage, server });
-
-    res.json({ isAuthentic });
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao verificar a assinatura', details: error.message });
-  }
-});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/verify', verifyDataTransactionRoute);
 
 const PORT = 3000;
 app.listen(PORT, () => {
