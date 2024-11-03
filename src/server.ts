283a20b58929968a82b7b5cd1e1556e135266ffb
Muller Esposito <mulleresposito@hotmail.com>
2024-11-03 02:57:48 -0300

    build(Vercel): it adds the api-docs route to the vercel config;

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,12 @@ import { verifyDataTransactionRoute } from './controllers/verifyDataTransactionC
 
 const app = express();
 app.use(bodyParser.json());
+const SWAGGER_CSS = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
+    customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: SWAGGER_CSS,
+}));
 app.use('/verify', verifyDataTransactionRoute);
 
 const PORT = 3000;
