8092d1b20829aff19c10ef01b2211e13faa0ebd4
Muller Esposito <mulleresposito@hotmail.com>
2024-11-03 02:40:03 -0300

    build(Vercel): it adds the api-docs route to the vercel config;

--- src/docs/swaggerConfig.ts
+++ src/docs/swaggerConfig.ts
@@ -1,4 +1,9 @@
 import swaggerJsDoc from 'swagger-jsdoc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const swaggerOptions = {
   swaggerDefinition: {
