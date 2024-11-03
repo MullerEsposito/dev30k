9dcf85e60b799eb8c22c4e68fa505fb80a5111f7
Muller Esposito <mulleresposito@hotmail.com>
2024-11-03 01:22:07 -0300

    feat(createServerService): it refacts the service to creates more types of servers instance;

--- src/services/createServerService.ts
+++ src/services/createServerService.ts
@@ -3,11 +3,16 @@ import * as StellarSdk from "@stellar/stellar-sdk"
 import { NetworksOptions } from "../utils/enums.js";
 
 export function createServerService(network: NetworksOptions) {
-  if (network == NetworksOptions.MAINNET) {
-    return new StellarSdk.rpc.Server('https://soroban-rpc.mainnet.stellar.gateway.fm');
-  } else if (network == NetworksOptions.TESTNET) {
-    return new StellarSdk.Horizon.Server('https://horizon-testnet.stellar.org');
-  } else {
-    throw new Error("Server type not supported");
+  switch (network) {
+    case NetworksOptions.SOROBAN_MAINNET:
+      return new StellarSdk.rpc.Server('https://soroban-rpc.mainnet.stellar.gateway.fm');
+    case NetworksOptions.SOROBAN_TESTNET:
+      return new StellarSdk.rpc.Server('https://soroban-testnet.stellar.org');
+    case NetworksOptions.HORIZON_MAINNET:
+      return new StellarSdk.Horizon.Server('https://horizon.stellar.org');
+    case NetworksOptions.HORIZON_TESTNET:
+      return new StellarSdk.Horizon.Server('https://horizon-testnet.stellar.org');
+    default:
+      throw new Error("Server type not supported");
   }
 }
