66828f9119e5816946036da218e8841638e5aa21
Muller Esposito <mulleresposito@hotmail.com>
2024-11-04 02:07:22 -0300

    docs(Swagger): it changes swagger docs to use the swagger-jsdoc library;

--- src/docs/swaggerDocs.ts
+++ src/docs/swaggerDocs.ts
@@ -1,23 +1,26 @@
-import { verifyPath } from '../controllers/verifyDataTransactionController/swagger.js';
+import swaggerJSDoc from "swagger-jsdoc";
 
-export const swaggerDocs = {
-  openapi: '3.0.0',
-  info: {
-    title: 'API de Verificação dos Dados de Transações',
-    version: '1.0.0',
-    description: 'Documentação da API para a rota de verificação dos dados de uma transação do DEV30K',
+const swaggerOptions = {
+  definition: {
+
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Verificação dos Dados de Transações',
+      version: '1.0.0',
+      description: 'Documentação da API para a rota de verificação dos dados de uma transação do DEV30K',
+    },
+    servers: [
+      {
+        url: 'https://dev30k.vercel.app',
+        description: 'Produção'
+      },
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor local'
+      },
+    ],
   },
-  servers: [
-    {
-      url: 'https://dev30k.vercel.app',
-      description: 'Produção'
-    },
-    {
-      url: 'http://localhost:3000',
-      description: 'Servidor local'
-    },
-  ],
-  paths: {
-  ...verifyPath
-  }
+  apis: ['src/**/*.ts']
 };
+
+export const swaggerDocs = swaggerJSDoc(swaggerOptions);
